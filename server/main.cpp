676f43e46827bbcb865afdd42f56ab40e6a0c6c0
Michal <czesio007p@gmail.com>
2021-01-04 23:10:07 +0100

    obsluga plikow

--- server/main.cpp
+++ server/main.cpp
@@ -16,6 +16,19 @@ void handleConnectionRequest(client_msg *clientAuthMsg, char **response);
 
 void handleOpenFileRequest(client_msg *clientAuthMsg, char **response);
 
+void handleReadFileRequest(client_msg *clientMsg, char **response) ;
+
+void handleWriteFileRequest(client_msg *clientMsg, char **response);
+
+void handleLSeekFileRequest(client_msg *clientMsg, char **response);
+
+void handleCloseFileRequest(client_msg *clientMsg, char **response);
+
+void handleUnlinkFileRequest(client_msg *clientMsg, char **response);
+
+void handleFstatFileRequest(client_msg *clientMsg, char **response);
+
+
 int main() {
 
     run_server(8080);
@@ -89,6 +102,24 @@ void getClientMsgFromSocketAndSendResponse(int new_socket) {
         case OPEN_FILE_REQUEST:
             handleOpenFileRequest(clientMsg, &response);
             break;
+        case READ_FILE_REQUEST:
+            handleReadFileRequest(clientMsg,&response);
+            break;
+        case WRITE_FILE_REQUEST:
+            handleWriteFileRequest(clientMsg,&response);
+            break;
+        case LSEEK_FILE_REQUEST:
+            handleLSeekFileRequest(clientMsg,&response);
+            break;
+        case CLOSE_FILE_REQUEST:
+            handleCloseFileRequest(clientMsg,&response);
+            break;
+        case UNLINK_FILE_REQUEST:
+            handleUnlinkFileRequest(clientMsg,&response);
+            break;
+        case FSTAT_FILE_REQUEST:
+            handleFstatFileRequest(clientMsg,&response);
+            break;
         default:
             perror("unknown request type");
             exit(EXIT_FAILURE);
@@ -123,18 +154,151 @@ void handleOpenFileRequest(client_msg *clientAuthMsg, char **response) {
     std::cout << "path : " << clientAuthMsg->arguments.open.path << "\n";
     std::cout << "oflag : " << clientAuthMsg->arguments.open.oflag << "\n";
     std::cout << "mode : " << clientAuthMsg->arguments.open.mode << "\n";
-
+    int32_t fd = open((char*))clientAuthMsg->arguments.open.path, clientAuthMsg->arguments.open.oflag , clientAuthMsg->arguments.open.mode);
     auto *openFileResponse = (server_msg *) malloc(sizeof(server_msg));
 
 
     openFileResponse->response_type = OPEN_FILE_RESPONSE;
     openFileResponse->response = {
             .open = {
-                    111111111,
+                    htonl(fd);
             }
+
     };
+    openFileResponse->error = htonl(errno);
+
     *response = (char *) openFileResponse;
     std::cout << "file is open" << std::endl;
 
 }
+
+void handleReadFileRequest(client_msg *clientMsg, char **response) {
+
+    std::cout<<"read file request"<<std::endl;
+    int fd = ntohl( clientMsg->arguments.read.fd);
+    int size  = ntohl(clientMsg->arguments.read.read_size);
+
+
+    char * buffer = (char*) malloc(size);
+
+    int32_t bytesRead = read(fd,buffer,size);
+    server_msg *serverMsg;
+    if(bytesRead>0)
+    {
+        serverMsg = (server_msg*) malloc(sizeof(server_msg ) + bytesRead);
+        memcpy(serverMsg->response.read.data,buffer,bytesRead);
+        serverMsg->response.read.data[bytesRead-1] = '\0';
+    } else  serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.read.size = htonl(bytesRead);
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = READ_FILE_RESPONSE;
+
+    std::cout<<"read: "<<serverMsg->response.read.data<< std::endl;
+    free(buffer);
+    *response = (char*)serverMsg;
+}
+
+void handleWriteFileRequest(client_msg *clientMsg, char **response) {
+
+    std::cout<<"write file request"<<std::endl;
+    int fd = ntohl( clientMsg->arguments.write.fd);
+    int size  = ntohl(clientMsg->arguments.write.write_size);
+
+    char * buffer = (char*) clientMsg->arguments.write.data;
+
+    ssize_t bytesWritten = write(fd,buffer,strlen(buffer));
+
+    auto *serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.write.size =htoln(bytesWritten);
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = WRITE_FILE_RESPONSE;
+
+
+    std::cout<<"write: "<<buffer<<"size: "<<serverMsg->response.write.size<<  std::endl;
+    *response = (char*)serverMsg;
+}
+
+void handleLSeekFileRequest(client_msg *clientMsg, char **response)
+{
+    std::cout<<"lseek file request"<<std::endl;
+    int fd = ntohl( clientMsg->arguments.lseek.fd);
+    off_t offset  = ntohl(clientMsg->arguments.lseek.offset);
+    int whence  = ntohl(clientMsg->arguments.lseek.whence);
+
+    off_t lseekOffset =  lseek( fd,  offset,  whence)
+    auto *serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.lseek.offset =htoln(lseekOffset);
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = LSEEK_FILE_RESPONSE;
+
+    std::cout<<"lseek offset:"<<serverMsg->response.lseek.offset<<  std::endl;
+    *response = (char*)serverMsg;
+}
+
+void handleCloseFileRequest(client_msg *clientMsg, char **response)
+{
+    std::cout<<"close file request"<<std::endl;
+    int fd = ntohl( clientMsg->arguments.close.fd);
+
+    int closeStatus = close(fd);
+
+    auto *serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.close.status = closeStatus;
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = CLOSE_FILE_RESPONSE;
+
+    std::cout<<"close status:"<<closeStatus <<  std::endl;
+    *response = (char*)serverMsg;
+}
+
+void handleUnlinkFileRequest(client_msg *clientMsg, char **response)
+{
+    std::cout<<"unlink file request"<<std::endl;
+
+
+    int closeStatus = unlink((char*)clientMsg->arguments.unlink.path);
+
+    auto *serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.unlink.status = closeStatus;
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = UNLINK_FILE_RESPONSE;
+
+    std::cout<<"unlink status:"<<closeStatus <<  std::endl;
+    *response = (char*)serverMsg;
+}
+
+void handleUnlinkFileRequest(client_msg *clientMsg, char **response)
+{
+    std::cout<<"fstat file request"<<std::endl;
+
+    struct stat buffer;
+    int         status;
+
+
+    status = fstat(fildes, &buffer);
+
+    int closeStatus = unlink((char*)clientMsg->arguments.unlink.path);
+
+    auto *serverMsg = (server_msg*) malloc(sizeof(server_msg ));
+
+    serverMsg->response.fstat.status = status;
+    serverMsg->response.fstat.buffer = buffer;
+    serverMsg->error = htonl(errno);
+    serverMsg->response_type = FSTAT_FILE_RESPONSE;
+
+    std::cout<<"fstat status:"<<status <<  std::endl;
+    *response = (char*)serverMsg;
+}
+
+
+struct stat buffer;
+int         status;
+
+
+
 

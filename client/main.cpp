9028f00fd30704edca8b6e0f6b4885b40a16d25f
Stasqq <stachocz@gmail.com>
2021-01-06 07:09:43 -0800

    blokada operacji w aplikacji klienta

--- client/main.cpp
+++ client/main.cpp
@@ -28,8 +28,8 @@ void changeConnection() {
     int connection_counter = 0;
     for(auto & connection : connections)
     {
-        connection_counter++;
         std::cout << connection_counter << ". " <<  connection.first << ":" << connection.second << std::endl;
+        connection_counter++;
     }
     std::cin >> connection_counter;
     changeCurrentConnection(connection_counter);
@@ -55,49 +55,53 @@ int printMenu() {
 
 void menuSwitch(int selectedOperation) {
     //todo::uzupelnic tego switcha
-    switch(selectedOperation){
-        case DIFFERENT_CONNECTION: {
-            changeConnection();
-        }break;
-        case CONNECTION_REQUEST: {
-            createConnection();
-        }break;
-        case OPEN_FILE_REQUEST: {
+    if(connections.empty() && (selectedOperation != CONNECTION_REQUEST && selectedOperation != EXIT_KEY)){
+        std::cout << "Nie jestes polaczony, ta operacja jest niedostepna!\n";
+    } else {
+        switch(selectedOperation){
+            case DIFFERENT_CONNECTION: {
+                changeConnection();
+            }break;
+            case CONNECTION_REQUEST: {
+                createConnection();
+            }break;
+            case OPEN_FILE_REQUEST: {
 
-        }break;
-        case WRITE_FILE_REQUEST: {
+            }break;
+            case WRITE_FILE_REQUEST: {
 
-        }break;
-        case LSEEK_FILE_REQUEST: {
+            }break;
+            case LSEEK_FILE_REQUEST: {
 
-        }break;
-        case CLOSE_FILE_REQUEST: {
+            }break;
+            case CLOSE_FILE_REQUEST: {
 
-        }break;
-        case UNLINK_FILE_REQUEST: {
+            }break;
+            case UNLINK_FILE_REQUEST: {
 
-        }break;
-        case FSTAT_FILE_REQUEST: {
+            }break;
+            case FSTAT_FILE_REQUEST: {
 
-        }break;
-        case OPEN_DIR_REQUEST: {
+            }break;
+            case OPEN_DIR_REQUEST: {
 
-        }break;
-        case READ_DIR_REQUEST: {
+            }break;
+            case READ_DIR_REQUEST: {
 
-        }break;
-        case CLOSE_DIR_REQUEST: {
+            }break;
+            case CLOSE_DIR_REQUEST: {
 
-        }break;
-        case DISCONNECT_REQUEST: {
-            disconnect();
-        }break;
-        case EXIT_KEY: {
+            }break;
+            case DISCONNECT_REQUEST: {
+                disconnect();
+            }break;
+            case EXIT_KEY: {
 
-        }break;
-        default:{
-            perror("menuSwitch default value");
-        }break;
+            }break;
+            default:{
+                perror("menuSwitch default value");
+            }break;
+        }
     }
 }
 
